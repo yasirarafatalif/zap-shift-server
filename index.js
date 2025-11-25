18c568aa995d8b311360cd7b1451cf6cd883f06c
Yasir Arafat Alif <yasirarafatalif1@gmail.com>
2025-11-25 22:34:58 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require('express')
+const cors =require('cors')
+const app = express()
+require('dotenv').config()
+const port = process.env.PORT|| 3000
+
+
+app.use(express.json())
+app.use(cors())
+
+
+
+// mongo db file  
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zgnatwl.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+
+    const db = client.db("zap-shift")
+    const percelSellCollcetion = db.collection("percel-Sell")
+
+    // percel sell api
+    app.get('/percel', async (req, res)=>{
+      const query ={}
+      const {email} = req.query;
+      if(email){
+        query.senderEmail=email
+      }
+      const curs = percelSellCollcetion.find(query)
+      const result = await curs.toArray()
+        res.send(result)
+    } )
+    app.post('/percel', async (req, res)=>{
+        const percel =req.body;
+        const result = await percelSellCollcetion.insertOne(percel)
+        res.send(result)
+        // console.log(result);
+    } )
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
