e7f0d0392ec131b74c52cfa2ec7453315906e019
Yasir Arafat Alif <yasirarafatalif1@gmail.com>
2025-11-27 23:15:56 +0600

    test payment add

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
-const cors =require('cors')
+const cors = require('cors')
 const app = express()
 require('dotenv').config()
-const port = process.env.PORT|| 3000
+
+// strip
+const stripe = require('stripe')(process.env.STRIPE_KEY);
+const port = process.env.PORT || 3000
 
 
 app.use(express.json())
@@ -26,49 +29,90 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
 
     const db = client.db("zap-shift")
     const percelSellCollcetion = db.collection("percel-Sell")
 
+    // all card show
+    app.get('/all-percel', async ( req, res)=>{
+      const result = await percelSellCollcetion.find().toArray();
+      res.send(result)
+      
+    })
+
     // percel sell api
-    app.get('/percel', async (req, res)=>{
-      const query ={}
-      const {email} = req.query;
-      if(email){
-        query.senderEmail=email
+    app.get('/percel', async (req, res) => {
+      const query = {}
+      const { email } = req.query;
+      if (email) {
+        query.senderEmail = email
       }
-      const options = {sort: {createAt: -1}}
+      const options = { sort: { createAt: -1 } }
       const curs = percelSellCollcetion.find(query, options)
       const result = await curs.toArray()
-        res.send(result)
-    } )
-
-    // singel data get
-    app.get("/percel/:id", async(req, res)=>{
-      const id = req.params.id;
-      const query = { _id : new ObjectId(id)}
-      const result = await  percelSellCollcetion.findOne(query)
       res.send(result)
     })
-    app.post('/percel', async (req, res)=>{
-        const percel =req.body;
-        percel.createAt= new Date ()
-        // sort 
-        
-        const result = await percelSellCollcetion.insertOne(percel)
-        res.send(result)
-        // console.log(result);
-    } )
+
+    // singel data get
+    app.get("/percel/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await percelSellCollcetion.findOne(query)
+      res.send(result)
+    })
+    app.post('/percel', async (req, res) => {
+      const percel = req.body;
+      percel.createAt = new Date()
+      // sort 
+
+      const result = await percelSellCollcetion.insertOne(percel)
+      res.send(result)
+      // console.log(result);
+    })
 
     //
-    app.delete('/percel/:id', async(req,res)=>{
+    app.delete('/percel/:id', async (req, res) => {
       const id = req.params.id
-      const query= {_id : new ObjectId(id)}
+      const query = { _id: new ObjectId(id) }
       const result = await percelSellCollcetion.deleteOne(query)
       // console.log(id);
       res.send(result)
+    })
+
+    // payment section
+    app.post('/create-checkout-session', async (req, res) => {
+      const paymentInfo = req.body;
+      console.log(paymentInfo);
+      const amount = parseInt(paymentInfo?.cost) * 100
+      console.log(amount);
+      const session = await stripe.checkout.sessions.create({
+        line_items: [
+          {
+            price_data: {
+              currency: "USD",
+              unit_amount: amount,
+              product_data: {
+                name: paymentInfo?.percelName
+              }
+            },
+
+            quantity: 1,
+          },
+        ],
+        customer_email: paymentInfo?.senderEmail,
+        mode: 'payment',
+        metadata: {
+          percelId: paymentInfo.percelId
+        },
+        success_url: `${process.env.MY_DOMAIN}dashboard/payment-success`,
+        cancel_url: `${process.env.MY_DOMAIN}dashboard/payment-canceled`,
+      });
+      
+
+      // console.log(session);
+      res.send({ url: session.url })
     })
 
 
